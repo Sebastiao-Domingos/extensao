3338cefe689910da7303c9fe65c1120b532550c9
Sebastiao-Domingos <domingossebastiao380@gmail.com>
2024-06-03 12:00:22 +0100

    first commit

---
+++ popup.js
@@ -0,0 +1,18 @@
+document.getElementById("changeColor").addEventListener("click", () => {
+  checkSSL();
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.scripting.executeScript({
+      target: { tabId: tabs[0].id },
+      func: () => (document.body.style.backgroundColor = "blue"),
+    });
+  });
+});
+
+function checkSSL() {
+  const url = new URL(window.location.href);
+  if (url.protocol === "https:") {
+    console.log("Conexão segura (HTTPS) detectada.");
+  } else {
+    console.log("Conexão não segura (HTTP) detectada.");
+  }
+}
